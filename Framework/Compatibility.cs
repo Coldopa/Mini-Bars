8081f0b044ef1e9ef16b7720735cae6089a7a306
Gustavo Prada Hinkel <prs.coldopa@gmail.com>
2025-01-09 12:37:37 -0300

    Added new and old files

---
+++ Framework/Compatibility.cs
@@ -0,0 +1,26 @@
+﻿using StardewValley;
+using StardewValley.Monsters;
+
+namespace MiniBars.Framework
+{
+    public class Compatibility
+    {
+        public static bool IsBlackListed()
+        {
+            if(ModEntry.instance.Helper.ModRegistry.IsLoaded("FlyingTNT.Swim") || ModEntry.instance.Helper.ModRegistry.IsLoaded("aedenthorn.Swim"))
+            {
+                if(Game1.player.currentLocation.Name.StartsWith("Custom_Underwater"))
+                {
+                    return true;
+                }
+            }
+            
+            if (Game1.player.currentLocation.Name == "Slime Hutch")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
